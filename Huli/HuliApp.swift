e5999065b7fa58b09012c30029ea6d5f90164ba3
Jia Huang <jiahuang2468@gmail.com>
2024-11-05 12:10:23 -0500

    Initial Commit

---
+++ Huli/HuliApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuliApp.swift
+//  Huli
+//
+//  Created by Jia Huang on 11/5/24.
+//
+
+import SwiftUI
+
+@main
+struct HuliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
